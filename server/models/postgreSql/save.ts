62e7d34353916175a39f394e3fae3075317d4097
Muzikanto <v89911112@mail.ru>
2019-06-19 16:49:28 +0300

    fix android all err

--- server/models/postgreSql/save.ts
+++ server/models/postgreSql/save.ts
@@ -6,22 +6,23 @@ const filmReader = new JSONReader({pathToData: ''});
 
 export const LoadFilmsFromJSON = async (path: string) => {
     const films = filmReader.read(path) as Array<{ name: string, id: string, date: string }>;
+    if (films) {
+        for (const v of films) {
+            try {
+                const {kinopoisk_id, title_ru, material_data: {genres, poster, studios}} = await moonRequest(v.id);
 
-    for (const v of films) {
-        try {
-            const {kinopoisk_id, title_ru, material_data: {genres, poster, studios}} = await moonRequest(v.id);
-
-            await CreateFilm({
-                id: kinopoisk_id,
-                studio: studios[0].length < 50 ? studios[0] : undefined,
-                name: title_ru,
-                date: v.date,
-                genres,
-                preview: poster,
-            });
-        } catch (e) {
-            if (e.message !== 'Duplicate Names')
-            console.log(v.name, e.message);
+                await CreateFilm({
+                    id: kinopoisk_id,
+                    studio: studios[0].length < 50 ? studios[0] : undefined,
+                    name: title_ru,
+                    date: v.date,
+                    genres,
+                    preview: poster,
+                });
+            } catch (e) {
+                if (e.message !== 'Duplicate Names')
+                    console.log(v.name, e.message);
+            }
         }
     }
 };
